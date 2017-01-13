dc263a20a8b483bb9bd4c24dbec60f116fa18e0a
fr34kyn01535 <fr34kyn01535@bam.yt>
2017-01-13 22:19:22 +0100

    ...

--- HomePlayer.cs
+++ HomePlayer.cs
@@ -25,7 +25,7 @@ namespace ZaupHomeCommand
 
         public void Awake()
         {
-            Logger.Log("Homeplayer is awake");
+            Rocket.Core.Logging.Logger.Log("Homeplayer is awake");
             Console.Write("Homeplayer is awake.");
         }
         protected override void Load()
@@ -35,7 +35,7 @@ namespace ZaupHomeCommand
         }
         public void GoHome(Vector3 bedPos, byte bedRot, UnturnedPlayer player)
         {
-            Logger.Log("starting gohome");
+            Rocket.Core.Logging.Logger.Log("starting gohome");
             this.waitrestricted = HomeCommand.Instance.Configuration.Instance.TeleportWait;
             this.movementrestricted = HomeCommand.Instance.Configuration.Instance.MovementRestriction;
             this.p = player;
@@ -62,7 +62,7 @@ namespace ZaupHomeCommand
                         List<RocketPermissionsGroup> hg = R.Permissions.GetGroups(player, true);
                         if (hg.Count <= 0)
                         {
-                            Logger.Log("There was an error as a player has no groups!");
+                            Rocket.Core.Logging.Logger.Log("There was an error as a player has no groups!");
                         }
                         byte[] time2 = new byte[hg.Count];
                         for (byte g=0;g<hg.Count;g++)
@@ -99,7 +99,7 @@ namespace ZaupHomeCommand
         }
         private void DoGoHome()
         {
-            Logger.Log("starting dogohome");
+            Rocket.Core.Logging.Logger.Log("starting dogohome");
             if (!this.cangohome) return;
             UnturnedChat.Say(this.p, String.Format(HomeCommand.Instance.Configuration.Instance.TeleportMsg, this.p.CharacterName));
             this.p.Teleport(this.bedPos, this.bedRot);
